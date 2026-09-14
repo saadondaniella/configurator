f3660da5f4e7e80073f87d1f8b7ecd77a9b8ce6c
Daniella Saadon <saadondaniella@gmail.com>
2026-09-14 12:27:44 +0200

    Add wrapper to all elements you can choose options on, and give it a fade in effect

--- src/component/StoryPanel.jsx
+++ src/component/StoryPanel.jsx
@@ -24,64 +24,136 @@ function StoryPanel({ form, setForm, color, setColor, size, setSize }) {
     <section className="story-panel">
       <p>Once upon a time, there was a girl who needed a pill.</p>
 
+      {/* FORM */}
       <div className="form-selection">
-        <p>
-          She wanted it to be{" "}
-          <button onClick={() => setShowFormOptions(!showFormOptions)}>
-            {form || "Choose"}
-          </button>
-        </p>
+        <span>She wanted it to be</span>
 
-        {showFormOptions && (
-          <div className="form-options">
-            <button onClick={() => handleFormSelect("capsule")}>Capsule</button>
-
-            <button onClick={() => handleFormSelect("round")}>Round</button>
-
-            <button onClick={() => handleFormSelect("heart")}>Heart</button>
-          </div>
-        )}
-      </div>
-
-      {form && (
-        <div className="color-selection">
-          <p>
-            Perfect. Now she needed it to be{" "}
-            <button onClick={() => setShowColorOptions(!showColorOptions)}>
-              {color || "Choose"}
+        <div className="form-choice">
+          {!showFormOptions && (
+            <button
+              className="text-button"
+              onClick={() => setShowFormOptions(true)}
+            >
+              {form || "Choose"}
             </button>
-          </p>
+          )}
 
-          {showColorOptions && (
-            <div className="color-options">
-              <button onClick={() => handleColorSelect("beige")}>Beige</button>
+          {showFormOptions && (
+            <div className="form-options fade-in">
+              <button
+                className="text-button"
+                onClick={() => handleFormSelect("capsule")}
+              >
+                Capsule
+              </button>
 
-              <button onClick={() => handleColorSelect("blue")}>Blue</button>
+              <button
+                className="text-button"
+                onClick={() => handleFormSelect("round")}
+              >
+                Round
+              </button>
 
-              <button onClick={() => handleColorSelect("red")}>Red</button>
+              <button
+                className="text-button"
+                onClick={() => handleFormSelect("heart")}
+              >
+                Heart
+              </button>
             </div>
           )}
         </div>
+
+        <span>because ordinary shapes were never really her thing.</span>
+      </div>
+
+      {/* COLOR */}
+      {form && (
+        <div className="color-selection fade-in">
+          <span>Of course, it had to be</span>
+
+          <div className="color-choice">
+            {!showColorOptions && (
+              <button
+                className="text-button"
+                onClick={() => setShowColorOptions(true)}
+              >
+                {color || "Choose"}
+              </button>
+            )}
+
+            {showColorOptions && (
+              <div className="color-options fade-in">
+                <button
+                  className="text-button"
+                  onClick={() => handleColorSelect("beige")}
+                >
+                  Beige
+                </button>
+
+                <button
+                  className="text-button"
+                  onClick={() => handleColorSelect("blue")}
+                >
+                  Blue
+                </button>
+
+                <button
+                  className="text-button"
+                  onClick={() => handleColorSelect("red")}
+                >
+                  Red
+                </button>
+              </div>
+            )}
+          </div>
+
+          <span>her favorite color.</span>
+        </div>
       )}
 
+      {/* SIZE */}
       {color && (
-        <div className="size-selection">
-          <p>
-            Now she needed to choose the size{" "}
-            <button onClick={() => setShowSizeOptions(!showSizeOptions)}>
-              {size || "Choose"}
-            </button>
-          </p>
+        <div className="size-selection fade-in">
+          <span>And since she liked to be prepared, she chose</span>
 
-          {showSizeOptions && (
-            <div className="size-options">
-              <button onClick={() => handleSizeSelect("2x3")}>2x3</button>
+          <div className="size-choice">
+            {!showSizeOptions && (
+              <button
+                className="text-button"
+                onClick={() => setShowSizeOptions(true)}
+              >
+                {size || "Choose"}
+              </button>
+            )}
 
-              <button onClick={() => handleSizeSelect("2x4")}>2x4</button>
+            {showSizeOptions && (
+              <div className="size-options fade-in">
+                <button
+                  className="text-button"
+                  onClick={() => handleSizeSelect("2x3")}
+                >
+                  2x3
+                </button>
 
-              <button onClick={() => handleSizeSelect("2x5")}>2x5</button>
-            </div>
-          )}
+                <button
+                  className="text-button"
+                  onClick={() => handleSizeSelect("2x4")}
+                >
+                  2x4
+                </button>
+
+                <button
+                  className="text-button"
+                  onClick={() => handleSizeSelect("2x5")}
+                >
+                  2x5
+                </button>
+              </div>
+            )}
+          </div>
+
+          <span>to make sure she had enough.</span>
         </div>
       )}
     </section>
